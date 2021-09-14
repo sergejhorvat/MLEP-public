edecfb788c01cf1442530b698eee7c42c2ccb9a5
Sergej Horvat <sergejhorvat@users.noreply.github.com>
2021-09-14 13:45:47 +0200

    w5

---
+++ course3/Resources/TensorFlow Lattice_files/devsite_devsite_panel_module.js
@@ -0,0 +1,1 @@
+(function(_ds){var window=this;'use strict';try{window.customElements.define(_ds.mi(),_ds.li)}catch(a){console.warn("Unrecognized DevSite custom element - DevsitePanel",a)};})(_ds_www);
---
+++ course3/Resources/TensorFlow Lattice_files/devsite_devsite_snackbar_module.js
@@ -0,0 +1,1 @@
+(function(_ds){var window=this;'use strict';try{window.customElements.define("devsite-snackbar",_ds.Xl)}catch(a){console.warn("Unrecognized DevSite custom element - DevsiteSnackBar",a)};})(_ds_www);
